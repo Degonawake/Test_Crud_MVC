3220d8088f899da574bd419b753252d5d535778f
Degonawake <diegoisgonzales@gmail.com>
2024-01-10 23:22:51 -0600

    Add project files.

---
+++ crud/Models/Entities/Students.cs
@@ -0,0 +1,13 @@
+﻿namespace crud.Models.Entities
+{
+    public class Students
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime Birthdate { get; set; }
+        public string ClassroomCode { get; set; }
+
+
+    }
+}
---
+++ crud/Models/Entities/StudentsContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace crud.Models.Entities
+{
+    public class StudentsContext : DbContext
+
+    {
+    
+
+
+    public StudentsContext(DbContextOptions<StudentsContext> options)
+        : base(options)
+    {
+
+    }
+
+
+    public DbSet<Students> Students { get; set; }
+
+}
+}
+
